06c1dab1c54aa4667028a2cead24b7a9df7a857f
JoseLamota <156946421+JoseLamota@users.noreply.github.com>
2024-10-15 00:56:35 -0400

    Update Cine.js

--- Cine.js
+++ Cine.js
@@ -1,42 +1,8 @@
-import { Familia } from './Familia.js';
-
-export class Cine {
-  constructor(cajaInicial) {
-    this.cajaInicial = cajaInicial;
-    this.familias = [];
-  }
-
-  agregarFamilia(familia) {
-    this.familias.push(familia);
-  }
-
-  obtenerFamiliaQuePagoMas() {
-    let familiaMayorPago = this.familias[0];
-    for (let i = 1; i < this.familias.length; i++) {
-      if (this.familias[i].calcularTotal() > familiaMayorPago.calcularTotal()) {
-        familiaMayorPago = this.familias[i];
-      }
+export default class Cl_mCine {
+    constructor(dineroinicial) { // Agregué el parámetro dineroinicial
+        this.dineroinicial = dineroinicial;
+        this.acummontoapagar = 0;
+        this.contdia = 0;
+        this.contdialunes = 0;
     }
-    return familiaMayorPago;
-  }
-
-  calcularPromedioLunes() {
-    let totalLunes = 0;
-    let familiasLunes = 0;
-    for (let i = 0; i < this.familias.length; i++) {
-      if (this.familias[i].dia === 1) {
-        totalLunes += this.familias[i].calcularTotal();
-        familiasLunes++;
-      }
-    }
-    return familiasLunes > 0 ? totalLunes / familiasLunes : 0;
-  }
-
-  calcularTotalFinal() {
-    let totalRecaudado = 0;
-    for (let i = 0; i < this.familias.length; i++) {
-      totalRecaudado += this.familias[i].calcularTotal();
-    }
-    return this.cajaInicial + totalRecaudado;
-  }
 }
